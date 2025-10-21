4ffaacaf6d71dd4d34bfc93bb1af37385cb0ab05
Rafiul Islam <www.rafiulislam1998@gmail.com>
2025-10-21 16:33:26 +0600

    make it sort and more perfect

--- Conditional/simple.js
+++ Conditional/simple.js
@@ -1,4 +1,4 @@
-let availableAmount = 200;
+let availableAmount = 6000;
 
 let expensiveBergerPrice = 550;
 let standardBergerPrice = 250;
@@ -13,11 +13,12 @@ wantToByeExpensiveBerger = availableAmount >= expensiveBergerPrice ? true : fals
 
 
 if (byeABerger) {
-    if (wantToByeExpensiveBerger && availableAmount >= expensiveBergerPrice) {
+    if (wantToByeExpensiveBerger) {
         if (availableAmount >= expensiveBergerPrice) {
             availableAmount -= expensiveBergerPrice;
             console.log("Here is your Expensive Berger and we have free sprite For you")
             console.log("And thats your return amount : " + availableAmount, "our Expensive Berger Price is : " + expensiveBergerPrice, "Thank you Sir , Have a nice day")
+            availableAmount <= 50 ? console.log( 'take it as a tip ') : console.log("Take 50 taka as a tip for your service ", availableAmount-= 50 ,"Sir thats your return Amount Take it " );
         }
     }
 
@@ -26,6 +27,7 @@ if (byeABerger) {
             availableAmount -= standardBergerPrice;
             console.log("Here is your Standard Berger and we have free candy For you")
             console.log("And thats your return amount : " + availableAmount, "our Standard Berger Price is : " + standardBergerPrice, "Thank you Sir , Have a nice day")
+            availableAmount <= 50 ? console.log( 'take it as a tip ') : console.log("Take 50 taka as a tip for your service ", availableAmount-= 50 ,"Sir thats your return Amount Take it " );
         }
     }
 
