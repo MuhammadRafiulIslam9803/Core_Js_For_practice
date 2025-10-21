ba0b034a92feff26c8f33b0554a9538301252c0a
Rafiul Islam <www.rafiulislam1998@gmail.com>
2025-10-21 16:14:11 +0600

    A Perfect Code for berger amount

--- Conditional/simple.js
+++ Conditional/simple.js
@@ -1,32 +1,35 @@
-let availableAmount = 0;
+let availableAmount = 200;
 
-const byeABerger = true;
-let ExpensiveBergerPrice = 550;
+let expensiveBergerPrice = 550;
 let standardBergerPrice = 250;
+let wantToByeExpensiveBerger;
+let byeABerger;
 
-const wantToByeExpensiveBerger = true
-const wantToByeStandardBerger = false
+
+availableAmount >= standardBergerPrice ? byeABerger = true : false;
+wantToByeExpensiveBerger = availableAmount >= expensiveBergerPrice ? true : false;
+
+
+
 
 if (byeABerger) {
-    if (wantToByeExpensiveBerger && availableAmount >= ExpensiveBergerPrice) {
-        if (availableAmount >= ExpensiveBergerPrice) {
-            availableAmount -= ExpensiveBergerPrice;
+    if (wantToByeExpensiveBerger && availableAmount >= expensiveBergerPrice) {
+        if (availableAmount >= expensiveBergerPrice) {
+            availableAmount -= expensiveBergerPrice;
             console.log("Here is your Expensive Berger and we have free sprite For you")
-            console.log("And thats your return amount : " + availableAmount, "our Expensive Berger Price is : " + ExpensiveBergerPrice, "Thank you Sir , Have a nice day")
+            console.log("And thats your return amount : " + availableAmount, "our Expensive Berger Price is : " + expensiveBergerPrice, "Thank you Sir , Have a nice day")
         }
     }
 
-    else if (wantToByeStandardBerger && availableAmount >= standardBergerPrice) {
+    else if (!wantToByeExpensiveBerger && availableAmount >= standardBergerPrice) {
         if (availableAmount >= standardBergerPrice) {
             availableAmount -= standardBergerPrice;
             console.log("Here is your Standard Berger and we have free candy For you")
             console.log("And thats your return amount : " + availableAmount, "our Standard Berger Price is : " + standardBergerPrice, "Thank you Sir , Have a nice day")
         }
     }
-    else {
-        console.log( availableAmount , ", In this price we Do not have any Berger for you sorry Sir Have a nice day" )
-    }
+
 }
-else{
-    console.log("Thanks for visiting , Have a nice day ")
+else {
+    console.log(availableAmount, ", In this price we Do not have any Berger for you sorry Sir Have a nice day")
 }
